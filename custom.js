dcf2f450e5c2289c7c01b9817ac2754a8a70b3b0
Leonardo Aristizabal <leonardo.aristizabal.m@gmail.com>
2023-06-20 21:21:58 -0300

    go to top buttons

--- custom.js
+++ custom.js
@@ -22,7 +22,7 @@ const navToggleEvent = function (elem) {
 navToggleEvent(navElemArr);
 navToggleEvent(navLinks);
 
-/* Header Sticky */
+/* Header Sticky and Go to Top */
 
 const header = document.querySelector("[data-header]");
 const goTopBtn = document.querySelector("[data-go-top]");
@@ -30,6 +30,7 @@ const goTopBtn = document.querySelector("[data-go-top]");
 window.addEventListener("scroll", function(){
     if (window.scrollY >= 200){
         header.classList.add("active");
+        goTopBtn.classList.add("active")
 
     } else{
         header.classList.remove("active");
